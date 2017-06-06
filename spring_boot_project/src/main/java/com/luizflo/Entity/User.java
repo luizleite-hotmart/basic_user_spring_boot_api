26251bd976d9b9e468009b31a9f7480a642bc83b
luizoliveira <xp.luiz@gmail.com>
2017-06-06 19:52:03 -0300

    add user improve somethings

---
+++ spring_boot_project/src/main/java/com/luizflo/Entity/User.java
@@ -0,0 +1,54 @@
+package com.luizflo.Entity;
+
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String email;
+    private String name;
+    @NotNull
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
