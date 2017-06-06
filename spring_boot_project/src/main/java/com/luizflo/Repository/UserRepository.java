2e71f5c5d6259881a1f2d08eb2158ae62bc39466
luizoliveira <xp.luiz@gmail.com>
2017-06-06 20:02:17 -0300

    create repository

---
+++ spring_boot_project/src/main/java/com/luizflo/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.luizflo.Repository;
+
+import com.luizflo.Entity.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    List<User> findByName(String name);
+
+}
