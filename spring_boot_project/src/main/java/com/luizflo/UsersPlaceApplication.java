9e020c680f598696e9b7158e7eaa3195fbd9899e
luizoliveira <xp.luiz@gmail.com>
2017-06-06 19:02:56 -0300

    initial from spring boot users project

---
+++ spring_boot_project/src/main/java/com/luizflo/UsersPlaceApplication.java
@@ -0,0 +1,12 @@
+package com.luizflo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersPlaceApplication.class, args);
+	}
+}
