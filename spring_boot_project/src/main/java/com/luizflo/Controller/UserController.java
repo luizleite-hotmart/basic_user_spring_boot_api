5bffa4bd5001d782ba1a3ce05767b26e9ee9b60a
luizoliveira <xp.luiz@gmail.com>
2017-07-15 08:01:17 -0300

    first_api

---
+++ spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
@@ -0,0 +1,22 @@
+package com.luizflo.Controller;
+
+import com.luizflo.Entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by luizoliveira on 15/07/17.
+ */
+
+@RestController
+public class UserController {
+
+    private static final String simple = "Lets GO!!! %s!";
+
+    @RequestMapping(method = RequestMethod.GET, path = "/entity")
+    public User simpleTest(@RequestParam(value = "name", defaultValue = "easy") String name) {
+        return new User("xp.luiz@gmail.com", name);
+    }
+}
