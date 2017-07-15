cb45b8afe4bd3efea8737831e55c57d858c41256
luizoliveira <xp.luiz@gmail.com>
2017-07-15 08:26:06 -0300

    add some fields on user

--- spring_boot_project/src/main/java/com/luizflo/Entity/User.java
+++ spring_boot_project/src/main/java/com/luizflo/Entity/User.java
@@ -19,11 +19,37 @@ public class User {
     private String name;
     @NotNull
     private String username;
+    private Boolean active = true;
+    private String ucode;
 
     public User(String email, String name) {
         this.email = email;
         this.name = name;
         this.username = name.replace(" ", ".");
+        this.ucode = String.valueOf(this.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!id.equals(user.id)) return false;
+        if (!email.equals(user.email)) return false;
+        if (!username.equals(user.username)) return false;
+        if (!active.equals(user.active)) return false;
+        return ucode.equals(user.ucode);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + active.hashCode();
+        return result;
     }
 
     public Long getId() {
@@ -55,6 +81,22 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getUcode() {
+        return ucode;
+    }
+
+    public void setUcode(String ucode) {
+        this.ucode = ucode;
     }
 
 }
