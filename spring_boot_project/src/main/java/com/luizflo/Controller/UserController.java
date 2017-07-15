f4574521116a863c121ffbfa882f639899689ef2
luizoliveira <xp.luiz@gmail.com>
2017-07-15 08:13:13 -0300

    add /users path to controller

--- spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
+++ spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/entity")
