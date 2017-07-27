8f90a8478b0505e9a2a32b9dc9fb7641df353478
luizoliveira <xp.luiz@gmail.com>
2017-07-26 22:09:00 -0300

    create service

--- spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
+++ spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 /**
  * Created by luizoliveira on 15/07/17.
  */
@@ -14,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+
     @RequestMapping(method = RequestMethod.GET, path = "/entity")
-    public User simpleTest(@RequestParam(value = "name", defaultValue = "easy") String name) {
+    public User returnUserModel(@RequestParam(value = "name", defaultValue = "easy") String name) {
         return new User("xp.luiz@gmail.com", name);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{userId}")
+    public User findUserById(@PathParam(value = "userId") Long userId) {
+
     }
 }
