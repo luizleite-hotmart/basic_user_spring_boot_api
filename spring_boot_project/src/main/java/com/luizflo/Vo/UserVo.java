3cc484c6f821fa5435061d6e5c0c6f269c407af7
luizoliveira <xp.luiz@gmail.com>
2017-07-29 16:18:38 -0300

    Create userVO entity

---
+++ spring_boot_project/src/main/java/com/luizflo/Vo/UserVo.java
@@ -0,0 +1,46 @@
+package com.luizflo.Vo;
+
+import javax.validation.constraints.NotNull;
+
+public class UserVo {
+
+    private Long id;
+    @NotNull
+    private String email;
+    @NotNull
+    private String name;
+    @NotNull
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
