6399bf5bc83fc9feb384f8b980125a9cea8aec4e
luizoliveira <xp.luiz@gmail.com>
2017-08-06 18:41:30 -0300

    remove unused import

--- spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
+++ spring_boot_project/src/main/java/com/luizflo/Controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
 /**
  * Created by luizoliveira on 15/07/17.
  */
