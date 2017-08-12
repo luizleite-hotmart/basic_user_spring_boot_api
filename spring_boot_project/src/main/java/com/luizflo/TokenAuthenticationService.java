9c44d5a9bca2e83bc06bd8bd10ac791a1d069391
luizleiteoliveira <luiz.leite@hotmart.com.br>
2017-08-12 14:56:35 -0300

    add get authentication method

--- spring_boot_project/src/main/java/com/luizflo/TokenAuthenticationService.java
+++ spring_boot_project/src/main/java/com/luizflo/TokenAuthenticationService.java
@@ -2,9 +2,14 @@ package com.luizflo;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+
+import static java.util.Collections.emptyList;
 
 /**
  * Created by luizleite on 12/08/17.
@@ -23,5 +28,22 @@ public class TokenAuthenticationService {
                 .signWith(SignatureAlgorithm.HS512, SECRET)
                 .compact();
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+    }
+
+    static Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        if (token != null) {
+            // parse the token.
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                    .getBody()
+                    .getSubject();
+
+            return user != null ?
+                    new UsernamePasswordAuthenticationToken(user, null, emptyList()) :
+                    null;
+        }
+        return null;
     }
 }
